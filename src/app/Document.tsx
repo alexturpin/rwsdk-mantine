1bb8bfd8a044938fabf54a5da987ffc926fd0713
Alex Turpin <alex.turpin@gmail.com>
2025-05-13 15:38:09 -0400

    CSS URL import

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -1,3 +1,5 @@
+import styles from "@mantine/core/styles.css?url"
+
 export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <html lang="en" data-mantine-color-scheme="dark">
     <head>
@@ -5,6 +7,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-minimal</title>
 
+      <link href={styles} rel="stylesheet"></link>
       <link rel="modulepreload" href="/src/client.tsx" />
     </head>
     <body>
